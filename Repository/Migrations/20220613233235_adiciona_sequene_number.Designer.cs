8322fd18020de29c8d3dc3c29340dc46e7658cfd
Rusley Santos <rusley.silva@uudi.com.br>
2022-06-13 20:40:13 -0300

    Adiciona sequence number

--- Repository/Migrations/20220613233235_adiciona_sequene_number.Designer.cs
+++ Repository/Migrations/20220613233235_adiciona_sequene_number.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository.Models;
 
 namespace Repository.Migrations
 {
     [DbContext(typeof(AnnotationContext))]
-    partial class AnnotationContextModelSnapshot : ModelSnapshot
+    [Migration("20220613233235_adiciona_sequene_number")]
+    partial class adiciona_sequene_number
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Repository/Migrations/20220613233235_adiciona_sequene_number.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class adiciona_sequene_number : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SEQUENCE_NUMBER",
+                table: "EXECUTION_TASK",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SEQUENCE_NUMBER",
+                table: "EXECUTION_TASK");
+        }
+    }
+}
